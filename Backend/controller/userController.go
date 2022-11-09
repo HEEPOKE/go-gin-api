51aa76a4431aefd9dbf538db5ac353e35ccd0959
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-11-09 12:10:39 +0700

    set db

---
+++ Backend/controller/userController.go
@@ -0,0 +1,31 @@
+package controller
+
+import (
+	"Backend/go-api/model"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:root@tcp(127.0.0.1:3306)/Shirtgo?charset=utf8&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&model.User{})
+
+	db.Create(&model.User{Firstname: "D42", Lastname: "a"})
+
+	var user model.User
+	db.First(&user, 1)
+	db.First(&user, "code = ?", "D42")
+
+	db.Model(&user).Update("Price", 200)
+
+	db.Model(&user).Updates(model.User{Firstname: "D42", Lastname: "a"})
+	db.Model(&user).Updates(map[string]interface{}{"Firstname": 200, "Lastname": "F42"})
+
+	db.Delete(&user, 1)
+}
--- Backend/main.go
+++ Backend/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/user", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
