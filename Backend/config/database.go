de96cfbd94abb01d79fefdc6b8d1fe2ae7118b66
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-11-10 11:08:35 +0700

    change

--- Backend/config/database.go
+++ Backend/config/database.go
@@ -1,1 +1,19 @@
 package config
+
+import (
+	"Backend/go-api/model"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func database() {
+	dsn := "root:root@tcp(127.0.0.1:3306)/Shirtgo?charset=utf8&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&model.User{})
+}
--- Backend/controller/authController.go
+++ Backend/controller/authController.go
@@ -1,27 +1,19 @@
 package controller
 
 import (
+	"Backend/go-api/config"
 	"Backend/go-api/model"
 	"net/http"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
 func register() {
-	dsn := "root:root@tcp(127.0.0.1:3306)/Shirtgo?charset=utf8&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
-	}
 
 	r := gin.Default()
 	r.Use(cors.Default())
-
-	db.AutoMigrate(&model.User{})
 
 	r.POST("/register", func(c *gin.Context) {
 		var json model.User
@@ -30,7 +22,7 @@ func register() {
 			return
 		}
 		var userExist model.User
-		db.Where("username = ?", json.Username).First(&userExist)
+		config.DB.Where("username = ?", json.Username).First(&userExist)
 		if userExist.ID > 0 {
 			c.JSON(http.StatusOK, gin.H{
 				"message": "user Exist",
@@ -41,7 +33,7 @@ func register() {
 
 		encryptedPassword, _ := bcrypt.GenerateFromPassword([]byte(json.Password), 10)
 		user := model.User{Username: json.Username, Password: string(encryptedPassword), Email: json.Email}
-		db.Create(&user)
+		config.DB.Create(&user)
 		if user.ID > 0 {
 			c.JSON(http.StatusOK, gin.H{
 				"userId":  user.ID,
