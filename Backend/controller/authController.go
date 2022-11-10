227d1fed419f9c468d2224aa7b432c8b6486817e
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-11-10 10:49:55 +0700

    register

--- Backend/controller/authController.go
+++ Backend/controller/authController.go
@@ -1,1 +1,58 @@
 package controller
+
+import (
+	"Backend/go-api/model"
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func register() {
+	dsn := "root:root@tcp(127.0.0.1:3306)/Shirtgo?charset=utf8&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	r := gin.Default()
+	r.Use(cors.Default())
+
+	db.AutoMigrate(&model.User{})
+
+	r.POST("/register", func(c *gin.Context) {
+		var json model.User
+		if err := c.ShouldBindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		var userExist model.User
+		db.Where("username = ?", json.Username).First(&userExist)
+		if userExist.ID > 0 {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "user Exist",
+				"status":  "error",
+			})
+			return
+		}
+
+		encryptedPassword, _ := bcrypt.GenerateFromPassword([]byte(json.Password), 10)
+		user := model.User{Username: json.Username, Password: string(encryptedPassword), Email: json.Email}
+		db.Create(&user)
+		if user.ID > 0 {
+			c.JSON(http.StatusOK, gin.H{
+				"userId":  user.ID,
+				"message": "success",
+				"status":  "ok",
+			})
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "fail",
+				"status":  "error",
+			})
+		}
+	})
+}
--- Backend/go.mod
+++ Backend/go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.8.1
+	golang.org/x/crypto v0.1.0
 	gorm.io/driver/mysql v1.4.3
 	gorm.io/gorm v1.24.1
 )
@@ -25,7 +26,6 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
-	golang.org/x/crypto v0.1.0 // indirect
 	golang.org/x/net v0.2.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
