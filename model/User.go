70638d499658654ae99b92471584fef508f1077f
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-03-08 09:19:18 +0700

    Update User.go

--- model/User.go
+++ model/User.go
@@ -4,9 +4,8 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	// Id       int64  `json:"id"`
-	Username string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 	Email    string `json:"email"`
 	Tel      string `json:"tel"`
 	Role     int    `json:"role"`
