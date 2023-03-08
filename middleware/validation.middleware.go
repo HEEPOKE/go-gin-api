e3779fa6fe7c725897037c2f0a55186a40f12d93
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-03-08 09:18:55 +0700

    Update validation.middleware.go

--- middleware/validation.middleware.go
+++ middleware/validation.middleware.go
@@ -11,16 +11,19 @@ import (
 )
 
 func ValidationUsers() gin.HandlerFunc {
+	hmacSampleSecret := []byte(os.Getenv("JWT_SECRET_KEY"))
+
 	return func(c *gin.Context) {
-		hmacSampleSecret := []byte(os.Getenv("JWT_SECRET_KEY"))
 		header := c.Request.Header.Get("Authorization")
 		tokenString := strings.Replace(header, "Bearer ", "", 1)
+
 		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("Signing Method: %v", token.Header["alg"])
+				return nil, fmt.Errorf("signing method: %v", token.Header["alg"])
 			}
 			return hmacSampleSecret, nil
 		})
+
 		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 			c.Set("userId", claims["userId"])
 		} else {
